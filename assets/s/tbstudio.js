624639e3a562036d5d14f3437c33aa78c0da8433
cobasaja1998 <61866512+cobasaja1998@users.noreply.github.com>
2020-03-06 16:55:36 +0700

    coba aja

---
+++ assets/s/tbstudio.js
@@ -0,0 +1,28 @@
+var game1 = document.getElementById('game1');
+var game2 = document.getElementById('game2');
+var game3 = document.getElementById('game3');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+
+game1.addEventListener('click', function(){
+    alert('game 1 belum tersedia');
+    // window.open('url here', '_BLANK');
+});
+
+game2.addEventListener('click', function(){
+    alert('game 2 belum tersedia');
+    // window.open('url here', '_BLANK');
+});
+
+game3.addEventListener('click', function(){
+    alert('game 3 belum tersedia');
+    // window.open('url here', '_BLANK');
+});
+
+prev.addEventListener('click', function(){
+    alert('tidak ada game lain tersedia');
+});
+
+next.addEventListener('click', function(){
+    alert('tidak ada game lain tersedia');
+});
